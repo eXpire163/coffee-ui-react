edc6957a5d577720074389919f8a07c3cbda4846
Christian Wiedemann-Gruber <christian.wiedemann@capgemini.com>
2022-04-03 17:10:18 +0200

    add darkmode switch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 
-import {  Grid } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import Header from './Header';
 import Content from './Content'
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+import { Paper, Switch } from '@material-ui/core/';
+import { useState } from 'react';
 
 const App = () => {
 
+  const [darkMode, setDarkMode] = useState(true)
 
+  function changeMode() {
+    setDarkMode(!darkMode)
+  }
+
+  const theme = createTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+    },
+  })
 
   return (
-    <Grid container direction='column'>
-      <Grid item >
-        <Header />
-      </Grid>
-      <Grid item container>
-        <Grid xs={false} sm={2}></Grid>
-        <Grid xs='12' sm='8'>
-          <Content />
-        </Grid>
-        <Grid xs={false} sm={2}></Grid>
-      </Grid>
+    <ThemeProvider theme={theme}>
+      <Paper>
+        <Grid container direction='column'>
+          <Grid item >
+            <Header changeMode={changeMode} isDarkMode={darkMode} />
+          </Grid>
+          <Grid item container>
+            <Grid xs={false} sm={2}></Grid>
+            <Grid xs='12' sm='8'>
+              <Content />
+            </Grid>
+            <Grid xs={false} sm={2}></Grid>
+          </Grid>
 
-    </Grid>
+        </Grid>
+      </Paper>
+
+    </ThemeProvider>
   );
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,26 +1,27 @@
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, Switch } from '@material-ui/core'
 import AcUnitRoundedIcon from '@material-ui/icons/AcUnitRounded';
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
 
 const useStyles = makeStyles({
-    typographyStyles:{
+    typographyStyles: {
         flex: 1,
     }
 });
 
 
-function Header() {
-  const classes = useStyles();
-  return (
-    <AppBar position='static'>
-        <Toolbar>
-              <Typography className={classes.typographyStyles}>This is the header</Typography>
-            <AcUnitRoundedIcon />
-        </Toolbar>
-    </AppBar>
-  )
+function Header(props) {
+    const classes = useStyles();
+    return (
+        <AppBar position='static'>
+            <Toolbar>
+                <Typography className={classes.typographyStyles}>This is the header {props.isDarkMode ? 'dark' : 'light'}</Typography>
+                <Switch checked={props.isDarkMode} onChange={props.changeMode} />
+                <AcUnitRoundedIcon />
+            </Toolbar>
+        </AppBar>
+    )
 }
 
 export default Header
